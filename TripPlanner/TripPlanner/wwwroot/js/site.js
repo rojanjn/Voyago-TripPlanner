44e73289149533ee8a6f3a218303c0907fb12890
dreamshrine <shayne.e.s.atkins@gmail.com>
2026-03-09 21:00:41 -0400

    Updated Index UI

--- TripPlanner/TripPlanner/wwwroot/js/site.js
+++ TripPlanner/TripPlanner/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function showTab(tabId)
+{
+    let tabs = document.querySelectorAll(".tab-content");
+    let buttons = document.querySelectorAll(".tab");
+
+    tabs.forEach(t => t.classList.remove("active"));
+    buttons.forEach(b => b.classList.remove("active"));
+
+    document.getElementById(tabId).classList.add("active");
+
+    event.target.classList.add("active");
+}
