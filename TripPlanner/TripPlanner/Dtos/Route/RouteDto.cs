0e4b59d4e0212e892adf71dda88f80edd3f66bfb
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-03-15 21:19:35 -0400

    Fix route response to return per-leg distance and duration

--- TripPlanner/TripPlanner/Dtos/Route/RouteDto.cs
+++ TripPlanner/TripPlanner/Dtos/Route/RouteDto.cs
@@ -3,6 +3,13 @@
 public class RouteDto
 {
     public string Polyline { get; set; } = "";
+    public string TotalDistance { get; set; } = "";
+    public string TotalDuration { get; set; } = "";
+    public List<LegDto> Legs { get; set; } = new();
+}
+
+public class LegDto
+{
     public string Distance { get; set; } = "";
     public string Duration { get; set; } = "";
 }
