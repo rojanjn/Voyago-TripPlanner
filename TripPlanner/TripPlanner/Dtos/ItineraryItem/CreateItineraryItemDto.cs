5ffc37a817a504347230d064526f3ab37a017818
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-03-15 21:23:02 -0400

    Fix end date can set earlier than start date

--- TripPlanner/TripPlanner/Dtos/ItineraryItem/CreateItineraryItemDto.cs
+++ TripPlanner/TripPlanner/Dtos/ItineraryItem/CreateItineraryItemDto.cs
@@ -17,4 +17,13 @@ public class CreateItineraryItemDto
     public int StopOrder { get; set; }
     
     public string? Note { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndDateTime <= StartDateTime)
+            yield return new ValidationResult(
+                "End time must be after start time",
+                new[] { nameof(EndDateTime) }
+            );
+    }
 }
