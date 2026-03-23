3a8c17c376bd43febb40f3fb26ca13fa60e222ec
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-03-22 21:18:19 -0400

    Delete migration for fixing deployment bug

--- TripPlanner/TripPlanner/Migrations/20260323011737_InitialCreate.cs
+++ TripPlanner/TripPlanner/Migrations/20260323011737_InitialCreate.cs
@@ -270,7 +270,20 @@ namespace TripPlanner.Migrations
                     { 1, "290 Bremner Blvd, Toronto, ON M5V 3L9", null, 43.6425662m, -79.3870568m, "CN Tower", null },
                     { 2, "Isfahan, Isfahan Province, Iran", null, 32.65745m, 51.677778m, "Naqsh-e Jahan Square", null },
                     { 3, "Yuchi Township, Nantou County, Taiwan", null, 23.866667m, 120.916667m, "Sun Moon Lake", null },
-                    { 4, "Tuojiang Town, Fenghuang County, Xiangxi Tujia and Miao Autonomous Prefecture of Hunan Province", null, 27.952822m, 109.600989m, "Fenghuang Ancient City", null }
+                    { 4, "Tuojiang Town, Fenghuang County, Xiangxi Tujia and Miao Autonomous Prefecture of Hunan Province", null, 27.952822m, 109.600989m, "Fenghuang Ancient City", null },
+                    { 5, "1 Austin Terrace, Toronto, ON M5R 1X8", null, 43.6781015m, -79.4094159m, "Casa Loma", null },
+                    { 6, "1 Blue Jays Way, Toronto, ON M5V 1J4", null, 43.6416598m, -79.3891976m, "Rogers Centre", null },
+                    { 7, "160 Kendal Ave, Toronto, ON M5R 1M3", null, 43.675865m, -79.4109118m, "George Brown Polytechnic - Casa Loma Campus", null },
+                    { 8, "200 King St E, Toronto, ON M5A 3W8", null, 43.6514195m, -79.3708678m, "George Brown Polytechnic - St. James Campus", null },
+                    { 9, "51 Dockside Dr, Toronto, ON M5A 0B6", null, 43.6441155m, -79.3656226m, "George Brown Polytechnic- Waterfront Campus", null },
+                    { 10, "100 Queens Park, Toronto, ON M5S 2C6", null, 43.6657043m, -79.3939679m, "Royal Ontario Museum", null },
+                    { 11, "317 Dundas St W, Toronto, ON M5T 1G4", null, 43.6536066m, -79.3925123m, "Art Gallery of Ontario", null },
+                    { 12, "288 Bremner Blvd, Toronto, ON M5V 3L9", null, 43.6424036m, -79.3868690m, "Ripley's Aquarium of Canada", null },
+                    { 13, "6650 Niagara Pkwy, Niagara Falls, ON L2G 0L0", null, 43.0799120m, -79.0746950m, "Niagara Falls (Horseshoe Falls)", null },
+                    { 14, "111 Wellington St, Ottawa, ON K1A 0A9", null, 45.4235937m, -75.7009290m, "Parliament Hill", null },
+                    { 15, "Old Quebec, Quebec City, QC G1R", null, 46.8138783m, -71.2079809m, "Old Quebec (Vieux-Québec)", null },
+                    { 16, "Vancouver, BC V6G 1Z4", null, 49.3043010m, -123.1443000m, "Stanley Park", null },
+                    { 17, "Banff, AB T1L 1K2", null, 51.4968460m, -115.9280560m, "Banff National Park", null }
                 });
 
             migrationBuilder.CreateIndex(
