9f1fcd3f7a28d957559b48c259315405d14d96fd
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-03-23 20:39:58 -0400

    Change the fields in itinerary item dto to fit what we need. Move the code to the correct controller to abide by MVC structure and modify them.

--- TripPlanner/TripPlanner/Dtos/ItineraryItem/UpdateItineraryItemDto.cs
+++ TripPlanner/TripPlanner/Dtos/ItineraryItem/UpdateItineraryItemDto.cs
@@ -4,7 +4,6 @@ namespace TripPlanner.Dtos.ItineraryItem;
 
 public class UpdateItineraryItemDto
 {
-    public int LocationId { get; set; }
     public DateTime StartDateTime { get; set; }
     public DateTime EndDateTime { get; set; }
     public int StopOrder { get; set; }
