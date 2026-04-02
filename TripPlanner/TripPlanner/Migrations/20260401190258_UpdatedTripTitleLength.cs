d22cdc4d4f1458cddc3a01a9230152e37993e640
dreamshrine <shayne.e.s.atkins@gmail.com>
2026-04-02 08:28:39 -0400

    trip title fixed

---
+++ TripPlanner/TripPlanner/Migrations/20260401190258_UpdatedTripTitleLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTripTitleLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "itineraries",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "itineraries",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
