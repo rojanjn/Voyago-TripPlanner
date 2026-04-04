f63e58b28a70a60dd30082d285a94bdd700990d0
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-04-04 18:26:48 -0400

    Add date validation in edit view. Limit the date(year) range from 2000-3000.

---
+++ TripPlanner/TripPlanner/wwwroot/js/itinerary-form.js
@@ -0,0 +1,17 @@
+﻿document.querySelector('.navbar')?.classList.add('scrolled');
+
+const startDate = document.querySelector('input[name="StartDate"]');
+const endDate = document.querySelector('input[name="EndDate"]');
+
+if (startDate && endDate) {
+    function validateDates() {
+        if (startDate.value && endDate.value) {
+            if (new Date(endDate.value) < new Date(startDate.value)) {
+                alert('End date cannot be before start date');
+                endDate.value = '';
+            }
+        }
+    }
+    startDate.addEventListener('change', validateDates);
+    endDate.addEventListener('change', validateDates);
+}
